a4a5cb3723d48d78e504ec498ad7e4bd28af34c3
Tusnádi Szabolcs <tusnadiszabolcs@gmail.com>
2025-11-04 09:28:21 +0100

    feat: __init__ factory added

---
+++ BudgetApp_backend/app/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from .extensions import db, bcrypt, cors
+from .models import User, Transaction
+from .routes import main_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    cors.init_app(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'database.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+
+    app.register_blueprint(main_bp)
+
+    return app
