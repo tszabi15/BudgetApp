9bfb17dc98317fdc277ea85c09a7faf172b894a0
Tusnádi Szabolcs <tusnadiszabolcs@gmail.com>
2025-11-04 09:59:14 +0100

    feat\!: JWT secret key added to backend app config

--- BudgetApp_backend/app/__init__.py
+++ BudgetApp_backend/app/__init__.py
@@ -12,6 +12,8 @@ def create_app():
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'database.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    app.config['SECRET_KEY'] = 'e76620c3772613d0be845166bc38173409ac529b55ee3c11'
 
     db.init_app(app)
     bcrypt.init_app(app)
