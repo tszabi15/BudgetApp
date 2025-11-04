99261b5807287d89a2b675ab0cfba448fad73845
Tusnádi Szabolcs <tusnadiszabolcs@gmail.com>
2025-11-04 10:09:58 +0100

    refactor: main_bp renamed to api_bp

--- BudgetApp_backend/app/__init__.py
+++ BudgetApp_backend/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from .extensions import db, bcrypt, cors
 from .models import User, Transaction
-from .routes import main_bp
+from .routes import api_bp
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +18,6 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
 
-    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
